6d6f00766dee588a81db4aae87045a7bf4394bdb
Horpschenzy <olanre@wallpostdev.com>
2021-04-12 04:28:38 +0100

    working on refined app

--- resources/views/emails/welcomemail.blade.php
+++ resources/views/emails/welcomemail.blade.php
@@ -129,7 +129,7 @@ a[x-apple-data-detectors] {
        <table class="es-content" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%"> 
          <tr style="border-collapse:collapse"> 
           <td align="center" style="padding:0;Margin:0"> 
-           <table class="es-content-body" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;width:600px"> 
+           <table class="es-content-body" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;width:600px">   
              <tr style="border-collapse:collapse"> 
               <td align="left" style="padding:0;Margin:0;padding-top:15px;padding-left:35px;padding-right:35px"> 
                <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"> 
