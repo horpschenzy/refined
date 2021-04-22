13f5c3cd12ca6770dc1ec3e20f9c020ae0ab7e61
Horpschenzy <olanre@wallpostdev.com>
2021-04-22 22:38:57 +0100

    working on admission

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('styles')
     <link href="admin/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
     <link href="admin/assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    
     <!-- Responsive datatable examples -->
     <link href="admin/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
 @endsection
--- resources/views/admin/layout/admin-app.blade.php
+++ resources/views/admin/layout/admin-app.blade.php
@@ -8,7 +8,7 @@
         <meta content="Themesbrand" name="author" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="dist/images/refined-new-black.png">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
         <!-- Bootstrap Css -->
         <link href="admin/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
         <!-- Icons Css -->
