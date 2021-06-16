d3dc9ee2245f10d1ec790d0a57757f8fc2ef9b69
Opeoluwa lanre <olanre@wallpostdev.com>
2021-06-16 14:36:01 +0100

    working on wexport

--- app/Exports/BulkExport.php
+++ app/Exports/BulkExport.php
@@ -3,32 +3,19 @@
 namespace App\Exports;
 
 use App\Models\Application;
-use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class BulkExport implements FromQuery,WithHeadings
+class BulkExport implements FromCollection, WithMapping, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function headings(): array
+
+    public function collection()
     {
-        return [
-            'Id',
-            'First Name',
-            'Last Name',
-            'Email',
-            'Phone Number',
-            'Telegram Phone Number',
-            'Country',
-            'State',
-            'Marital Status',
-            'Gender',
-        ];
-    }
-    public function query()
-    {
-        return Application::query()->with('user')->whereRaw('add_to_count = 1')->where('status', 'approved')->select('id','firstname','lastname','email','phone','telegram_phone','country','state','maritalstatus','gender');
+        return Application::query()->with('user')->whereRaw('add_to_count = 1')->where('status', 'approved')->get();
         /*you can use condition in query to get required result
          return Bulk::query()->whereRaw('id > 5');*/
     }
@@ -39,6 +26,7 @@ class BulkExport implements FromQuery,WithHeadings
             $applicant->id,
             $applicant->firstname,
             $applicant->lastname,
+            $applicant->user->reg_no,
             $applicant->email,
             $applicant->phone,
             $applicant->telegram_phone,
@@ -46,6 +34,23 @@ class BulkExport implements FromQuery,WithHeadings
             $applicant->state,
             $applicant->maritalstatus,
             $applicant->gender,
+        ];
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Id',
+            'First Name',
+            'Last Name',
+            'Red No.',
+            'Email',
+            'Phone Number',
+            'Telegram Phone Number',
+            'Country',
+            'State',
+            'Marital Status',
+            'Gender',
         ];
     }
 
