578f7baf9e90c344ecbbce463dd6ce05802cde5e
Babatope Oni <babatopeoni@gmail.com>
2023-06-28 17:33:16 +0100

    Fix migrations

---
+++ database/migrations/2023_06_28_161748_fix_truncated_columns.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('resources', function (Blueprint $table) {
+            $table->longText('url')->nullable()->change();
+        });
+
+        Schema::table('applications', function (Blueprint $table) {
+            $table->longText('ministry')->nullable()->change();
+        });
+
+        
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('resources', function (Blueprint $table) {
+            $table->string('url', 255)->change();
+        });
+
+        Schema::table('applications', function (Blueprint $table) {
+            $table->string('ministry')->nullable()->change();
+        });
+
+    }
+};
